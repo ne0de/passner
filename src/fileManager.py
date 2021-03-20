ad77006f45fc59f5298c281697c60f12ec3e5666
Manuel Cabral <cabral.manuel@yandex.com>
2021-03-20 19:39:22 -0300

    create dir an exceptions

--- src/fileManager.py
+++ src/fileManager.py
@@ -1,15 +1,16 @@
-import os, platform
+import os, sys, platform
 
 class PassnerFileManager:
     def __init__(self):
         self.__system = platform.system()
-        self.__dirs = { 
-            'Windows' : 'C:/Program Files/Passner',
-            'Linux' : '/home/User/Passner'
-        }
+        self.__dirs = { 'Windows' : 'C:/Program Files/Passner', 'Linux' : '/home/User/Passner' }
+        self.__cdir = self.__dirs[self.__system]
     
-    def search(self): return os.path.exists(self.__dirs[self.__system])
+    def existDir(self): return os.path.exists(self.__cdir)
 
     def createDir(self):
-        pass
-            
+        try:
+            os.mkdir(self.__cdir)
+            return True
+        except OSError as exc:
+            return False, exc.strerror
