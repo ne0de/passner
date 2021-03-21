1ffb64be5c9104b57fda55238c10deaa85049746
Manu <cabral.manuel@yandex.com>
2021-03-21 07:12:02 -0300

    interpreter for linux

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
+#! /usr/bin/python3
+
 import sys, ctypes, platform, os
+
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QInputDialog, QLineEdit
 from dialog import Ui_MainWindow
 from fileManager import PassnerFileManager
