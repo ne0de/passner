f59eaa25f7a8d655920217dd75f8b5f762cf11a4
Manuel Cabral <cabral.manuel@yandex.com>
2021-03-21 00:31:38 -0300

    get dir

--- src/fileManager.py
+++ src/fileManager.py
@@ -6,11 +6,15 @@ class PassnerFileManager:
         self.__dirs = { 'Windows' : 'C:/Program Files/Passner', 'Linux' : '/home/User/Passner' }
         self.__cdir = self.__dirs[self.__system]
     
+    def getDir(self): return self.__cdir
+
     def existDir(self): return os.path.exists(self.__cdir)
+
+    def existArchiveData(self): return os.path.exists(self.__cdir + '/data')
 
     def createDir(self):
         try:
             os.mkdir(self.__cdir)
-            return True
+            return True, 'Directorio creado'
         except OSError as exc:
             return False, exc.strerror
